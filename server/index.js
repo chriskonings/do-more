167745a15d7ca943b6d7df8c63b757dfad16e2e9
Chris Konings <chriskonings@CK.local>
2018-07-10 17:38:41 -0400

    set up for heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const express = require('express')
-const app = express()
+const serveStatic = require('serve-static')
+const path = require('path')
 const yelp = require('yelp-fusion');
 const config = require('../config.js');
 const client = yelp.client(config.keys.YELP);
+const app = express()
+
+app.use("/", serveStatic ( path.join (__dirname, '../spa/dist')))
+
+// app.get('*', function (req, res) {
+//   res.sendFile(__dirname + '../spa/dist/index.html')
+// })
 
 app.get('/api', function(req, res) {
   const {lat, lng, term, radius, sortBy, offset} = req.query
@@ -43,5 +51,5 @@ app.get('/api', function(req, res) {
   return search
 })
 
-app.listen(3000, () => console.log('Listening on port 3000'))
-
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log('Listening on port 3000'))
