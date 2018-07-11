63d7ae5ee4f980ea95ae7a2b68dff36eb170c740
Chris Konings <chriskonings@CK.local>
2018-07-10 22:16:24 -0400

    added port alternative for prod

--- index.js
+++ index.js
@@ -58,5 +58,6 @@ app.get('/api', function(req, res) {
   return search
 })
 
-const port = 3000
-app.listen(port, () => console.log('Listening on port 3000'))
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('Listening on port 3000'));
